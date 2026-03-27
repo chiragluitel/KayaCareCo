f47f7b3b12501ec7d3d767a4ea9c1beec9b02fcd
prod-chirag <chiragluitel32@gmail.com>
2026-03-28 10:12:18 +1100

    Initial Setup v1.0.0

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div> 
+      <h1 className='text-5xl'> Kaya Care Co</h1>
+    </div>
+  )
+}
+
+export default App
