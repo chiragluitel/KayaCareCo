c8c3b142124709cb9604dfef9f22201cc8c4f842
prod-chirag <chiragluitel32@gmail.com>
2026-04-03 11:57:58 +1100

    link changes

--- frontend/src/data/footerdata.ts
+++ frontend/src/data/footerdata.ts
@@ -7,10 +7,12 @@ export const QUICK_LINKS = [
   ];
   
 export const SERVICE_LINKS = [
-    { label: 'Nursing & High Intensity Care', path: '/services/nursing-care' },
-    { label: 'Development of Life Skills', path: '/services/life-skills' },
+    { label: 'Nursing & High Intensity Care', path: '/services/nursing-intensive-care' },
+    { label: 'Continence Management', path: '/services/continence-mgmt' },
+    { label: 'Supported Mental Health Services', path: '/services/mental-health' },
+    { label: 'Supported Independed Living', path: '/services/supported-independent-living' },
+    { label: 'Household Tasks', path: '/services/household-tasks' },
     { label: 'In-Home Support & Personal Care', path: '/services/in-home-support' },
-    { label: 'Community Nursing', path: '/services/community-nursing' },
-    { label: 'Domestic Assistance', path: '/services/domestic-assistance' },
+    { label: 'Life Skills & Capacity Building', path: '/services/life-skills-training' },
   ];
   
